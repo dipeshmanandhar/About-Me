8ceb31ad67f3c81b3e4a429d2ebc47093dbb0ab6
Dipesh Manandhar <dipesh_manz@yahoo.com>
2020-01-03 18:49:01 -0500

    added nickname field

--- app/src/main/java/com/udacity/aboutme/MainActivity.kt
+++ app/src/main/java/com/udacity/aboutme/MainActivity.kt
@@ -1,12 +1,36 @@
 package com.udacity.aboutme
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.done_button).setOnClickListener {
+            addNickname(it)
+        }
+    }
+
+    private fun addNickname(doneButton : View) {
+        val nicknameEdit : EditText = findViewById(R.id.nickname_edit)
+        val nicknameText : TextView = findViewById(R.id.nickname_text)
+
+        nicknameText.text = nicknameEdit.text
+        nicknameEdit.visibility = View.GONE
+        doneButton.visibility = View.GONE
+        nicknameText.visibility = View.VISIBLE
+
+        // hides the keyboard
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(doneButton.windowToken, 0)
     }
 }
