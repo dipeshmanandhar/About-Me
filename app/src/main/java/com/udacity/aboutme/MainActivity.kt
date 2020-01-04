4173bd13c1777d0f1b15f05fcec445ba073df9a8
Dipesh Manandhar <dipesh_manz@yahoo.com>
2020-01-03 21:12:30 -0500

    added data binding

--- app/src/main/java/com/udacity/aboutme/MainActivity.kt
+++ app/src/main/java/com/udacity/aboutme/MainActivity.kt
@@ -5,32 +5,36 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.InputMethodManager
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.udacity.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
+    private val myName: MyName = MyName("Dipesh Manandhar")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        findViewById<Button>(R.id.done_button).setOnClickListener {
+        binding.myName = myName
+
+        binding.doneButton.setOnClickListener {
             addNickname(it)
         }
     }
 
-    private fun addNickname(doneButton : View) {
-        val nicknameEdit : EditText = findViewById(R.id.nickname_edit)
-        val nicknameText : TextView = findViewById(R.id.nickname_text)
-
-        nicknameText.text = nicknameEdit.text
-        nicknameEdit.visibility = View.GONE
-        doneButton.visibility = View.GONE
-        nicknameText.visibility = View.VISIBLE
-
-        // hides the keyboard
-        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        imm.hideSoftInputFromWindow(doneButton.windowToken, 0)
+    private fun addNickname(view: View) {
+        binding.apply {
+            myName?.nickname = nicknameEdit.text.toString()
+            invalidateAll()
+            nicknameEdit.visibility = View.GONE
+            view.visibility = View.GONE
+            nicknameText.visibility = View.VISIBLE
+        }
+            // hides the keyboard
+            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
